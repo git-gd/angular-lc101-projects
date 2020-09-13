febf080a9726078019f83b9b0acf9a2dfe938439
Randy Parker <randallbparker@gmail.com>
2020-09-13 14:52:13 -0500

    before 28 7 3 part 2 add more comps

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://animationfascination.files.wordpress.com/2014/05/shrek178puss.jpg';
+  image3 = 'https://entertainment.time.com/wp-content/uploads/sites/3/2011/08/07_top10timetravelmovies.jpg';
 
   constructor() { }
 
