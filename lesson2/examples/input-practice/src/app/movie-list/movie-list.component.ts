c58cd2027f39f6575348c7bcacf8d5f759c298db
Randy Parker <randallbparker@gmail.com>
2020-09-14 13:47:32 -0500

    29 8 bonus completion

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+
+   validMovie = true;
+
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
 
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie(newTitle: string){
+      if(!this.movies.includes(newTitle.trim()) && newTitle.trim().length > 0){
+         this.movies.push(newTitle.trim());
+         this.validMovie = true;
+      } else {
+         this.validMovie = false;
+      }
    }
 }
