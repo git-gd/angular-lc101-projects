eca8bb882e9d4c2df4685affc02cfe893b36ce22
Randy Parker <randallbparker@gmail.com>
2020-09-15 14:41:38 -0500

    bonus complete with a minor Y axis oddity

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,12 +12,19 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  farRight = false;
+  farTop = false;
 
   takeOffEnabled: boolean = true;
 
-  inDanger(rocketImage, nbr){
-    // 50000 for temp value.. need parent values to correct
-    if (nbr > 50000) {
+  inDanger(rocketImage){
+    this.farRight = parseInt(rocketImage.style.left) >= (rocketImage.parentElement.clientWidth - rocketImage.clientWidth);
+    this.farTop = parseInt(rocketImage.style.bottom) >= (rocketImage.parentElement.clientHeight - rocketImage.clientHeight);
+    // ... farTop stops before the rocket leaves but not the background
+    // ... down lets the rocket travel one step below its start point but remains in bound
+    // the height of the rocket seems off
+
+    if (this.farRight || this.farTop) {
       rocketImage.style.background = "orange";
     } else {
       rocketImage.style.background = null;
@@ -28,7 +35,7 @@ export class AppComponent {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
     if (result) {
       this.color = 'blue';
-      this.height = 10000;
+      this.height = 10000; // shouldn't the shuttle image move up?
       this.width = 0;
       this.message = 'Shuttle in flight.';
       this.takeOffEnabled = false;
@@ -63,22 +70,22 @@ export class AppComponent {
       case 'right':
         rocketImage.style.left = rocketX + 10 + 'px';
         this.width += 10000;
-        this.inDanger(rocketImage, this.width);
+        this.inDanger(rocketImage);
         break;
       case 'left':
         rocketImage.style.left = rocketX - 10 + 'px';
         this.width -= 10000;
-        this.inDanger(rocketImage, this.width);
+        this.inDanger(rocketImage);
         break;
       case 'up':
         rocketImage.style.bottom = rocketY + 10 + 'px';
         this.height += 10000;
-        this.inDanger(rocketImage, this.height);
+        this.inDanger(rocketImage);
         break;
       case 'down':
         rocketImage.style.bottom = rocketY - 10 + 'px';
         this.height -= 10000;
-        this.inDanger(rocketImage, this.height);
+        this.inDanger(rocketImage);
         break;          
       }
   }
